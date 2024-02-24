ce84413c032a4d87591b0e4a577269b0ba593cbe
Jon Gunderson <jongund@illinois.edu>
2024-02-24 15:42:18 -0800

    added logo

--- gen-documentation.js
+++ gen-documentation.js
@@ -23,8 +23,36 @@ function outputFile(fname, data) {
   })
 }
 
-// Create index file
+const files = [
+  { template: './src-docs/templates/content-index.njk',
+    title: 'Open Web Accessibility Consulting',
+    filename: 'index.html'
+  },
+  { template: './src-docs/templates/content-about.njk',
+    title: 'About',
+    filename: 'about.html'
+  },
+  { template: './src-docs/templates/content-projects.njk',
+    title: 'Projects',
+    filename: 'services.html'
+  },
+  { template: './src-docs/templates/content-resources.njk',
+    title: 'Resources',
+    filename: 'resources.html'
+  },
+  { template: './src-docs/templates/content-services.njk',
+    title: 'Services',
+    filename: 'services.html'
+  }
+  ];
 
+// Create files
+
+files.forEach( f => {
+  outputFile(f.filename, nunjucks.render(f.template, {title: f.title}));
+})
+
+/*
 const htmlIndex = nunjucks.render('./src-docs/templates/content-index.njk',
   {title: 'Open Web Accessibility Consulting'});
 outputFile('index.html', htmlIndex);
@@ -40,3 +68,4 @@ outputFile('services.html', htmlServices);
 const htmlProjects = nunjucks.render('./src-docs/templates/content-projects.njk',
   {title: 'Projects'});
 outputFile('projects.html', htmlProjects);
+*/
