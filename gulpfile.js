79e49cc1133db8b2a6984efc6dbd54b039694274
Jon Gunderson <jongund@illinois.edu>
2024-02-24 16:26:08 -0800

    added sass file to build stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,22 @@ const gulp         = require('gulp');
 const exec         = require('child_process').exec;
 const {src, task}  = require('gulp');
 const {series}     = require('gulp');
+const sass         = require('gulp-sass')(require('sass'));
 
 gulp.task('documentation', function (cb) {
   exec('node ./gen-documentation.js', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
-    cb(err);
-  });
+    cb(err);  });
 })
 
-const documentation    = task('documentation');
+gulp.task('style', function () {
+  return gulp.src('./src-docs/templates/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./docs/css'));
+});
 
-exports.default = series(documentation);
+const documentation = task('documentation');
+const style         = task('style');
+
+exports.default = series(documentation, style);
