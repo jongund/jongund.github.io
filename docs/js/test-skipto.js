e632ed03bf3183a250d7c260136200a006e1b41b
Jon Gunderson <jongund@illinois.edu>
2025-08-13 11:45:41 -0500

    text skipto

--- docs/js/test-skipto.js
+++ docs/js/test-skipto.js
@@ -11,14 +11,40 @@ function handleFocusout(event) {
 }
 
 function handleFocus(event) {
-  status.value="Button got focus";
+  status.value=`"${event.currentTarget.textContent}" got focus`;
 }
 
 function handleBlur(event) {
-  event.currentTarget.classList.remove('focus');
-  status.value="Button lost focus";
+  status.value=`"${event.currentTarget.textContent}" lost focus`;
 }
 
+ class buttonTest extends HTMLElement {
+
+
+
+  constructor() {
+    // Always call super first in constructor
+    super();
+    this.attachShadow({ mode: 'open' });
+
+    const linkNode = document.createElement('link');
+    linkNode.rel = 'stylesheet';
+    linkNode.href = './css/test-skipto.css';
+    this.shadowRoot.appendChild(linkNode);
+
+    this.btn = document.createElement('button');
+    this.btn.textContent = 'Test Button';
+    this.btn.className = 'popup';
+    this.shadowRoot.appendChild(this.btn);
+
+    this.btn.addEventListener('focusin', handleFocusin);
+    this.btn.addEventListener('focusout', handleFocusout);
+    this.btn.addEventListener('focus', handleFocus);
+    this.btn.addEventListener('blur', handleBlur);
+  }
+}
+
+window.customElements.define('button-test', buttonTest);
 
 window.addEventListener("load", (event) => {
 
