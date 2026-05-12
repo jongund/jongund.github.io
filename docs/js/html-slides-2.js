7cb7d5cac39f69fb3bd51fa97197b80273671e39
Jon Gunderson <jongund@illinois.edu>
2026-05-12 10:49:44 -0500

    updated presentations

--- docs/js/html-slides-2.js
+++ docs/js/html-slides-2.js
@@ -290,6 +290,171 @@ class HTMLSlide {
 
 }
 
+/**
+ * @constructor SourceCode
+ *
+ * @desc  Creates source code of an example
+ *
+ * @property  location      Array  -  Object containing the keyCodes used by the slider widget
+ * @property  code          Array  -  JQuery node object
+ */
+
+SourceCode = function () {
+  this.location = new Array();
+  this.code = new Array();
+};
+
+/**
+ * @method add
+ *
+ * @desc  Adds source code
+ */
+
+SourceCode.prototype.add = function (locationId, codeId) {
+  this.location[this.location.length] = locationId;
+  this.code[this.code.length] = codeId;
+};
+
+/**
+ * @method make
+ *
+ * @desc  Generates HTML content for source code
+ */
+
+SourceCode.prototype.make = function () {
+
+  let nodeCode;
+  let nodeLocation;
+
+  for (let i = 0; i < this.location.length; i++) {
+
+    nodeLocation = document.getElementById(this.location[i]);
+    nodeCode = document.getElementById(this.code[i]);
+
+    nodeLocation.className = 'sourcecode';
+    this.createCode(nodeLocation, '', nodeCode, true);
+
+  } // endfor
+
+};
+
+/**
+ * @method createCode
+ *
+ * @desc  Specify the source code and the location of the source code
+ *
+ * @param  location   String   - id of element to put the source code
+ * @param  spaces     String   - Any spaces to precede the source code
+ * @param  node       Object   - DOM Element node to use to generate the source code
+ */
+
+SourceCode.prototype.createCode = function (location, spaces, node, first) {
+
+  function hasText (s) {
+    if (typeof s !== 'string') {return false;}
+
+    for (var i = 0; i < s.length; i++) {
+      let c = s[i];
+      if (c !== ' ' && c !== '\n' && c !== '\r') {return true;}
+    }
+    return false;
+  }
+
+  function cleanText (s) {
+    if (typeof s !== 'string') {return '';}
+
+    s1 = '';
+    for (let i = 0; i < s.length; i++) {
+      const c = s[i];
+
+      if (c === '<') {
+        c = '&lt;';
+      }
+
+      if (c === '>') {
+        c = '&gt;';
+      }
+
+      s1 += c;
+    }
+    return s1;
+  }
+
+  let i, s;
+  let count = 0;
+
+  if (typeof first !== 'boolean') {
+    first = false;
+  }
+
+  if (!first) {
+    let nodeNameStr = node.nodeName.toLowerCase();
+
+    location.innerHTML = location.innerHTML + '<br/>' + spaces + '&lt;' + nodeNameStr;
+
+    for (i = 0; i < node.attributes.length; i++) {
+
+      location.innerHTML = location.innerHTML + '&nbsp;' + node.attributes[i].nodeName + '="';
+      location.innerHTML = location.innerHTML + node.attributes[i].value + '"';
+
+      if (((i + 1) != node.attributes.length) && (node.attributes.length > 2)) {
+
+        location.innerHTML = location.innerHTML + '<br/>' + spaces;
+
+        for (let j = 2; j <= nodeNameStr.length; j++) {
+          location.innerHTML = location.innerHTML + '&nbsp;';
+        }
+
+      } // endif
+
+    } // endfor
+
+    location.innerHTML = location.innerHTML + '&gt;';
+  }
+
+  for (i = 0; i < node.childNodes.length; i++) {
+
+    let n = node.childNodes[i];
+
+    switch (n.nodeType) {
+
+      case Node.ELEMENT_NODE:
+        this.createCode(location, spaces + '&nbsp;&nbsp;', n);
+        count++;
+        break;
+
+      case Node.TEXT_NODE:
+        if (hasText(n.nodeValue)) {
+          s = cleanText(n.nodeValue);
+          location.innerHTML = location.innerHTML + '<br/>' + spaces + '&nbsp;&nbsp;' + s;
+        }
+        count++;
+        break;
+
+      case Node.COMMENT_NODE:
+
+        if (hasText(n.nodeValue)) {
+          location.innerHTML = location.innerHTML  + '<br/>' + spaces + '&nbsp;&nbsp;' + '&lt;--&nbsp;&nbsp;' + n.nodeValue + '--&gt;';
+        }
+        count++;
+        break;
+
+    } // end switch
+
+  } // end for
+
+  if (!first) {
+    if (count > 0) {
+      location.innerHTML = location.innerHTML + '<br/>' + spaces + '&lt;/' + node.nodeName.toLowerCase();
+      location.innerHTML = location.innerHTML + '&gt;';
+    } // end if
+  }
+
+};
+
+var sourceCode = new SourceCode();
+
+
 window.addEventListener( 'load', () => {
   new HTMLSlide();
 });
