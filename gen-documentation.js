1b7ecc7df130a0b047c485c9641de9eb4dcfd428
Jon Gunderson <jongund@illinois.edu>
2026-09-05 13:01:15 -0500

    update menu navigation and added system color resource

--- gen-documentation.js
+++ gen-documentation.js
@@ -8,9 +8,119 @@ const nunjucks  = require('nunjucks');
 
 /* Constants */
 
-const outputDirectory = './docs/';
+const tagLineName = "Web Accessibility Consulting, Workshops and Free Tools for You";
 
-// setUseCodeTags(true);
+const outputDirectory   = './docs/';
+
+const websiteURL        = 'https://opwenweba11y.com';
+
+const mainPages = [
+  { content:  'content-index.njk',
+    title:    'Home',
+    link:     'Home',
+    filename: 'index.html'
+  },
+  {
+    dropdown: 'Outreach',
+    id: 'id-outreach',
+    pages: [
+      { content:  'content-sessions.njk',
+        title:    'Workshops',
+        link:     'Workshops',
+        filename: 'workshops.html'
+      },
+      { content:  'content-presentations.njk',
+        title:    'Presentations',
+        link:     'Presentations',
+        filename: 'Presentation.html'
+      }
+    ]
+  },
+  { content:  'content-consulting.njk',
+    title:    'Consulting and Mentoring',
+    link:     'Consulting',
+    filename: 'consulting.html'
+  },
+  { content:  'content-standards.njk',
+    title:    'Standards',
+    link:     'Standards',
+    filename: 'standards.html'
+  },
+  {
+    dropdown: 'Tools',
+    id: 'id-tools',
+    pages: [
+      { content: 'content-tools.njk',
+        title: 'Open-Source Tools',
+        link: 'Open-Source Tools',
+        filename: 'tools.html'
+      },
+      { content: 'content-system-colors.njk',
+        title: 'System Colors',
+        link: 'System Colors',
+        filename: 'system-colors.html'
+      }
+    ]
+  },
+  { content: 'content-about.njk',
+    title: 'About',
+    link: 'About',
+    filename: 'about.html'
+  }
+ ];
+
+// Create content files
+
+function createNavigation(pages) {
+  console.log(`[create Navigation]`);
+  let html = '\n';
+  pages.forEach( item => {
+    console.log(`[create Navigation]: ${item.dropdown} ${item.filename}`);
+    if (item.dropdown) {
+      html += `
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle"
+             data-bs-toggle="dropdown"
+            href="#"
+            role="button"
+            aria-expanded="false"
+            aria-controls="${item.id}">${item.dropdown}</a>
+          <ul class="dropdown-menu" id="${item.id}">`;
+
+      item.pages.forEach( p => {
+        console.log(`[dropdown][page]: ${p.filename}`);
+        if (p.filename) {
+          html += `<li><a class="dropdown-item" href="${p.filename}">${p.link}</a></li>`;
+        }
+        else {
+          if (p.url) {
+          html += `<li><a class="dropdown-item" href="${p.url}">${p.link}</a></li>`;
+          }
+          else {
+            html += `<li><hr class="dropdown-divider"></li>`;
+          }
+        }
+      });
+
+      html += `
+          </ul>
+        </li>
+      `;
+    }
+    else {
+      html += `
+        <li class="nav-item">
+          <a class="nav-link" href="${item.filename}">${item.link}</a>
+        </li>
+      `;
+    }
+  });
+  html += '\n';
+
+  return html;
+}
+
+const mainNav = createNavigation(mainPages);
 
 /* Helper functions */
 
@@ -23,75 +133,43 @@ function outputFile(fname, data) {
   })
 }
 
-const files = [
-  { template: './src-docs/templates/content-index.njk',
-    title: 'Home',
-    menuName: 'Home',
-    filename: 'index.html'
-  },
-  { template: './src-docs/templates/content-workshops.njk',
-    title: 'Presentations and Workshops',
-    menuName: 'Workshops',
-    filename: 'workshops.html'
-  },
-  { template: './src-docs/templates/content-consulting.njk',
-    title: 'Consulting and Mentoring',
-    menuName: 'Consulting',
-    filename: 'consulting.html'
-  },
-  { template: './src-docs/templates/content-tools.njk',
-    title: 'Open-Source Tools',
-    menuName: 'Tools',
-    filename: 'tools.html'
-  },
-  { template: './src-docs/templates/content-standards.njk',
-    title: 'Standards',
-    menuName: 'Standards',
-    filename: 'standards.html'
-  },
-  { template: './src-docs/templates/content-about.njk',
-    title: 'About',
-    menuName: 'About',
-    filename: 'about.html'
-  },
-  { template: './src-docs/templates/content-sessions.njk',
-    title: 'Sessions',
-    menuName: '',
-    filename: 'sessions/index.html'
-  },
-  { template: './src-docs/templates/content-presentations.njk',
-    title: 'Presentations',
-    menuName: '',
-    filename: 'presentations/index.html'
-  },
-  { template: './src-docs/templates/content-test-hc.njk',
-    title: 'High Contrast Test',
-    menuName: '',
-    filename: 'test-hc.html'
+function createPage(page, mainNav, dropdownName='', dropdownPages=false) {
+  if (page.filename) {
+    console.log(`  [createPage]: ${page.filename}`);
+
+    outputFile(page.filename,
+      nunjucks.render('./src-docs/templates/page.njk',{
+        content: page.content,
+        navigation: mainNav,
+        dropdownName: dropdownName,
+        dropdownPages: dropdownPages,
+        websiteURL: websiteURL,
+        tagLineName: tagLineName,
+        title: page.title
+      })
+    );
   }
-  ];
+}
 
-// Create files
-files.forEach( f => {
-  const p = f.filename.includes('/') ? '../' : './';
-  outputFile(f.filename, nunjucks.render(f.template, {title: f.title, files: files, path: p}));
-  console.log(`${f.filename} ${p}`);
-})
 
-/*
-const htmlIndex = nunjucks.render('./src-docs/templates/content-index.njk',
-  {title: 'Open Web Accessibility Consulting'});
-outputFile('index.html', htmlIndex);
+// createPages(supportPages);
 
-const htmlAbout = nunjucks.render('./src-docs/templates/content-about.njk',
-  {title: 'About'});
-outputFile('about.html', htmlAbout);
+function createPages(pages) {
+  console.log(`[create pages]`);
+  pages.forEach( item => {
+    if (item.dropdown) {
+      item.pages.forEach( p => {
+        createPage(p, mainNav, item.dropdown, item.pages);
+      });
+    }
+    else {
+      createPage(item, mainNav);
+    }
+  });
+}
 
-const htmlServices = nunjucks.render('./src-docs/templates/content-services.njk',
-  {title: 'Services'});
-outputFile('services.html', htmlServices);
+createPages(mainPages);
 
-const htmlProjects = nunjucks.render('./src-docs/templates/content-projects.njk',
-  {title: 'Projects'});
-outputFile('projects.html', htmlProjects);
-*/
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const gulp         = require('gulp');
 const exec         = require('child_process').exec;
 const {src, task}  = require('gulp');
 const {series}     = require('gulp');
-const sass         = require('gulp-sass')(require('sass'));
 
 gulp.task('documentation', function (cb) {
   exec('node ./gen-documentation.js', function (err, stdout, stderr) {
@@ -11,13 +10,7 @@ gulp.task('documentation', function (cb) {
     cb(err);  });
 })
 
-gulp.task('style', function () {
-  return gulp.src('./src-docs/templates/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./docs/css'));
-});
-
 const documentation = task('documentation');
 const style         = task('style');
 
-exports.default = series(documentation, style);
+exports.default = series(documentation);
